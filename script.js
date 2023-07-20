8a35c7dbc743bed6584f95b0e35f84c98020e4fb
Kanna116 <137594947+Kanna116@users.noreply.github.com>
2023-07-20 18:36:26 +0530

    project-3,4 added and loco js

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
 var tl = gsap.timeline();
 
 
 tl.from(".profile-image,.profile-details h1, .profile-details h3, .profile-details p, .profile-socials", {
-    y: 100,
+    y: 150,
     opacity: 0,
-    stagger: .3
+    stagger: .3,
+    duration: .8
+})
+
+tl.from("#projects", {
+    width: 0,
+    duration: .5
 })
 tl.from(".item", {
     y: 100,
     opacity: 0,
     duration: .8,
-    stagger: .2
+    stagger: .3
 })
